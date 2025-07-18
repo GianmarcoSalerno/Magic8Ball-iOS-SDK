0f7a1b7b838066768243e4b6acb20f9e5f0c6765
Gianmarco Salerno <gianmarco.m.salerno@gmail.com>
2025-07-18 09:53:02 -0500

    Initial Commit: Working basic package

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 6.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Magic8Ball-iOS-SDK",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "Magic8Ball-iOS-SDK",
+            targets: ["Magic8Ball-iOS-SDK"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "Magic8Ball-iOS-SDK"),
+        .testTarget(
+            name: "Magic8Ball-iOS-SDKTests",
+            dependencies: ["Magic8Ball-iOS-SDK"]
+        ),
+    ]
+)
