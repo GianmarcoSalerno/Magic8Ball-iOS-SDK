a6feb6b7fd67af2daad8c8c318a3c8fa8e332ac6
Gianmarco Salerno <gianmarco.m.salerno@gmail.com>
2025-07-18 10:09:18 -0500

    feat: Implement basic Magic8BallView SDK
    - Create SwiftUI UIViewRepresentable wrapper for WKWebView
    - Load Magic 8-Ball web experience from provided URL
    - Provide simple public API: Magic8BallView()
    - Enable one-line integration for host apps

--- Sources/Magic8Ball-iOS-SDK/Magic8Ball_iOS_SDK.swift
+++ Sources/Magic8Ball-iOS-SDK/Magic8Ball_iOS_SDK.swift
@@ -1,2 +1,24 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import SwiftUI
+import WebKit
+
+public struct Magic8BallView: UIViewRepresentable {
+    private let urlString = "https://xvehl58kcb.appflowapp.com/chat"
+
+    public init() {}
+
+    public func makeUIView(context: Context) -> WKWebView {
+        let webView = WKWebView()
+        if let url = URL(string: urlString) {
+            let request = URLRequest(url: url)
+            webView.load(request)
+        }
+        return webView
+    }
+
+    public func updateUIView(_ uiView: WKWebView, context: Context) {
+        // No-op for now
+    }
+}
